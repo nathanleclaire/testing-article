247fcd02194c60dfc8f6a21f585af88376f8d489
Nathan LeClaire <nathan.leclaire@gmail.com>
2015-10-12 11:57:50 -0700

    Fix final test case
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- example3/job_test.go
+++ example3/job_test.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 	"time"
-
-	"github.com/docker/machine/libmachine/log"
 )
 
-type WriteOnceLogger struct {
-	msg string
+type ReadableLogger interface {
+	Logger
+	Read() string
 }
 
-func (wol *WriteOnceLogger) Log(args ...interface{}) {
-	log.Warn(args)
-	wol.msg = fmt.Sprint(args...)
+type MessageReader struct {
+	Msg string
+}
+
+func (mr *MessageReader) Read() string {
+	return mr.Msg
+}
+
+type LastEntryLogger struct {
+	*MessageReader
+}
+
+func (lel *LastEntryLogger) Log(args ...interface{}) {
+	lel.Msg = fmt.Sprint(args...)
+}
+
+type DiscardFirstWriteLogger struct {
+	*MessageReader
+	writtenBefore bool
+}
+
+func (dfwl *DiscardFirstWriteLogger) Log(args ...interface{}) {
+	if dfwl.writtenBefore {
+		dfwl.Msg = fmt.Sprint(args...)
+	}
+	dfwl.writtenBefore = true
 }
 
 type FakeServerPoller struct {
@@ -28,49 +51,56 @@ func (fsp FakeServerPoller) PollServer() (string, error) {
 
 func TestPollerJobRunLog(t *testing.T) {
 	defaultTestWaitInterval := 100 * time.Millisecond
-	_ = 20 * time.Millisecond
+	shortInterval := 20 * time.Millisecond
 	longInterval := 200 * time.Millisecond
 
 	testCases := []struct {
 		expectedMsg string
 		p           PollerJob
+		logger      ReadableLogger
 		sp          ServerPoller
 	}{
-		/*
-			{
-				expectedMsg: "200 OK",
-				p:           NewPollerJob("madeup.website", shortInterval),
-				sp:          FakeServerPoller{"200 OK", nil},
-			},
-			{
-				expectedMsg: "500 SERVER ERROR",
-				p:           NewPollerJob("down.website", shortInterval),
-				sp:          FakeServerPoller{"500 SERVER ERROR", nil},
-			},
-			{
-				expectedMsg: "Error trying to get state: DNS probe failed",
-				p:           NewPollerJob("error.website", shortInterval),
-				sp:          FakeServerPoller{"", errors.New("DNS probe failed")},
-			},
-		*/
+		{
+			expectedMsg: "200 OK",
+			p:           NewPollerJob("madeup.website", shortInterval),
+			logger:      &LastEntryLogger{&MessageReader{}},
+			sp:          FakeServerPoller{"200 OK", nil},
+		},
+		{
+			expectedMsg: "500 SERVER ERROR",
+			p:           NewPollerJob("down.website", shortInterval),
+			logger:      &LastEntryLogger{&MessageReader{}},
+			sp:          FakeServerPoller{"500 SERVER ERROR", nil},
+		},
+		{
+			expectedMsg: "Error trying to get state: DNS probe failed",
+			p:           NewPollerJob("error.website", shortInterval),
+			logger:      &LastEntryLogger{&MessageReader{}},
+			sp:          FakeServerPoller{"", errors.New("DNS probe failed")},
+		},
 		{
 			expectedMsg: "",
 			p:           NewPollerJob("some.website", longInterval),
-			sp:          FakeServerPoller{"200 OK", nil},
+
+			// Discard first write since we want to verify that no
+			// additional logs get made after the first one (time
+			// out)
+			logger: &DiscardFirstWriteLogger{MessageReader: &MessageReader{}},
+
+			sp: FakeServerPoller{"200 OK", nil},
 		},
 	}
 
 	for _, c := range testCases {
-		logger := &WriteOnceLogger{}
-		c.p.Logger = logger
+		c.p.Logger = c.logger
 		c.p.ServerPoller = c.sp
 
 		go c.p.Run()
 
 		time.Sleep(defaultTestWaitInterval)
 
-		if logger.msg != c.expectedMsg {
-			t.Errorf("Expected message did not align with what was written:\n\t%q != %q", c.expectedMsg, logger.msg)
+		if c.logger.Read() != c.expectedMsg {
+			t.Errorf("Expected message did not align with what was written:\n\t%q != %q", c.expectedMsg, c.logger.Read())
 		}
 	}
 }
